5239045dc0635d1b00b150d162615466b76560a9
Raphaël Calabro <ddaeke-github@yahoo.fr>
2022-01-04 08:32:28 +0100

    SpriteDefinition now extends Named

---
+++ MeliceFramework/SpriteDefinition.swift
@@ -0,0 +1,12 @@
+//
+//  SpriteDefinition.swift
+//  MeliceFramework
+//
+//  Created by Raphaël Calabro on 03/01/2022.
+//
+
+import Foundation
+
+extension MELSpriteDefinition: Named {
+    public static let defaultName = "A sprite definition"
+}
